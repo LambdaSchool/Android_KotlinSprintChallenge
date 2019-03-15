9f66e03b688e970e0aa78ea7a6a5235283598ed4
Jacob Seppanen <jbseppanen@gmail.com>
2019-03-15 08:19:31 -0700

    Add NetworkAdapter

---
+++ app/src/main/java/com/jbseppanen/android_kotlinsprintchallenge/NetworkAdapter.kt
@@ -0,0 +1,80 @@
+package com.jbseppanen.android_kotlinsprintchallenge
+
+import android.support.annotation.WorkerThread
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+
+object NetworkAdapter {
+    const val GET = "GET"
+    const val POST = "POST"
+    const val PUT = "PUT"
+    const val DELETE = "DELETE"
+    const val TIMEOUT = 3000
+
+    @WorkerThread
+    fun httpRequest(stringUrl: String, requestType: String, jsonBody: String?,headerProperties: Map<String, String>? = null): Pair<Boolean, String> {
+            var result = ""
+            var success = false
+            var stream: InputStream? = null
+            var connection: HttpURLConnection? = null
+            try {
+                val url = URL(stringUrl)
+                connection = url.openConnection() as HttpURLConnection
+                connection.readTimeout = TIMEOUT
+                connection.connectTimeout = TIMEOUT
+                connection.requestMethod = requestType
+                connection.setRequestProperty("Content-Type", "application/json")
+
+                if (headerProperties != null) {
+                    for((key, value) in headerProperties) {
+                        connection.setRequestProperty(key, value)
+                    }
+                }
+
+                if (requestType == GET || requestType == DELETE) {
+                    connection.connect()
+                } else if (requestType == POST || requestType == PUT) {
+                    if (jsonBody != null) {
+                        val outputStream = connection.outputStream
+                        outputStream.write(jsonBody.toByteArray())
+                        outputStream.close()
+                    }
+                }
+
+                if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                    stream = connection.inputStream
+                    if (stream != null) {
+                        val reader = BufferedReader(InputStreamReader(stream))
+                        val builder = StringBuilder()
+                        var line: String? = reader.readLine()
+                        while (line != null) {
+                            builder.append(line)
+                            line = reader.readLine()
+                        }
+                        result = builder.toString()
+                        success = true
+                    }
+                }
+
+            } catch (e: MalformedURLException) {
+                e.printStackTrace()
+                result = e.message.toString()
+            } catch (e: IOException) {
+                e.printStackTrace()
+                result = e.message.toString()
+            } finally {
+                connection?.disconnect()
+
+                if (stream != null) {
+                    try {
+                        stream.close()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        return success to result
+    }
+}
