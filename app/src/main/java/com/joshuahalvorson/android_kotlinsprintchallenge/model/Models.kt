9d90f16ec7ec7002075fdd6e2550781346d3ddc3
Josh Halvorson <joshhalvorson16@gmail.com>
2019-03-15 09:01:38 -0700

    Add default values

--- app/src/main/java/com/joshuahalvorson/android_kotlinsprintchallenge/model/Models.kt
+++ app/src/main/java/com/joshuahalvorson/android_kotlinsprintchallenge/model/Models.kt
@@ -5,31 +5,31 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Video(
-    val id: Number?,
-    val name: String?,
-    val image: String?,
-    val collection: String?
+    val id: Number? = -1,
+    val name: String? = "",
+    val image: String? = "",
+    val collection: String? = ""
 )
 
 @Serializable
 data class VideoDetails(
-    val name: String?,
-    val short_description: String?,
-    val youtube_id: String?,
-    val credits: String?,
-    val mission: String?,
-    val collection: String?,
-    val image: String?,
-    val image_retina: String?,
-    val video_files: List<VideoFiles>?
+    val name: String? = "",
+    val short_description: String? = "",
+    val youtube_id: String? = "",
+    val credits: String? = "",
+    val mission: String? = "",
+    val collection: String? = "",
+    val image: String? = "",
+    val image_retina: String? = "",
+    val video_files: List<VideoFiles>? = listOf<VideoFiles>()
 )
 
 @Serializable
 data class VideoFiles(
-    val file_url: String?,
-    val file_size: Number?,
-    val width: Number?,
-    val height: Number?,
-    val frame_rate: String?,
-    val format: String?
+    val file_url: String? = "",
+    val file_size: Number? = -1,
+    val width: Number? = -1,
+    val height: Number? = -1,
+    val frame_rate: String? = "",
+    val format: String? = ""
 )
