a9f2d56ddc040f0c87da2af6f4acd3497f789440
Jacob Seppanen <jbseppanen@gmail.com>
2019-03-15 08:42:15 -0700

    Update activity_main layout.

--- app/src/main/java/com/jbseppanen/android_kotlinsprintchallenge/MainActivity.kt
+++ app/src/main/java/com/jbseppanen/android_kotlinsprintchallenge/MainActivity.kt
@@ -5,8 +5,14 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val videoUrl = "https://media.stsci.edu/uploads/video_file/video_attachment/4976/STScI-H-v1845a-640x360.mp4"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
