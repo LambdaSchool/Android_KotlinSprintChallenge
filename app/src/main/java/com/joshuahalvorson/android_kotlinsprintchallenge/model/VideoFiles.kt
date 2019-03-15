99f1efbd4e1e666671bb4f32dbc454518f932228
Josh Halvorson <joshhalvorson16@gmail.com>
2019-03-15 09:48:31 -0700

    Change to int

--- app/src/main/java/com/joshuahalvorson/android_kotlinsprintchallenge/model/VideoFiles.kt
+++ app/src/main/java/com/joshuahalvorson/android_kotlinsprintchallenge/model/VideoFiles.kt
@@ -5,9 +5,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class VideoFiles(
     val file_url: String? = "",
-    val file_size: Number? = -1,
-    val width: Number? = -1,
-    val height: Number? = -1,
+    val file_size: Int? = -1,
+    val width: Int? = -1,
+    val height: Int? = -1,
     val frame_rate: String? = "",
     val format: String? = ""
 )
