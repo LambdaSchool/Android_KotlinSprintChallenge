869fa5b70a197bb44e733a212aa4e3ed5aeeb686
Pablo Ogata <pabloogata@gmail.com>
2019-04-05 19:37:28 -0400

    fixed as instraction

--- app/src/main/java/com/example/android_kotlinsprintchallenge/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlinsprintchallenge/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.example.android_kotlinsprintchallenge
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.VideoView
 import com.example.android_kotlinsprintchallenge.model.Video
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
 
 class MainActivity : AppCompatActivity() {
 
-    val dataJob = Job()
-    val dataScope = CoroutineScope(Dispatchers.IO + dataJob)
+    companion object {
+        const val url = "http://hubblesite.org/api/v3/video/1200"
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,50 +31,55 @@ class MainActivity : AppCompatActivity() {
         val seekbar = findViewById<SeekBar>(R.id.seekBar)
         val videoView = findViewById<VideoView>(R.id.player_video)
 
+        val dataJob = Job()
+        val dataScope = CoroutineScope(Dispatchers.IO + dataJob)
 
+        suspend fun getVideos() {
+            val (fileSuccess, fileResult) = NetworkAdapter.httpGetRequest(
+                "http://hubblesite.org/api/v3/video/1200"
+            )
+            if (fileSuccess) {
+                val video = Json.nonstrict.parse(Video.serializer(), fileResult)
+                withContext(Dispatchers.Main) {
+                    player_video.setVideoURI(Uri.parse(video.getVideoFileUrl()))
+                }
+            }
+        }
 
-       buttonPause.setOnClickListener{
+        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    player_video.seekTo(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+        buttonPause.setOnClickListener {
             val isPlaying = videoView.isPlaying
-           if(isPlaying){
-               videoView.pause()}
-           else{
-               videoView.start()
-           }
+            if (isPlaying) {
+                videoView.pause()
+            } else {
+                videoView.start()
+            }
+        }
 
-        //   buttonNext.text = "skip"
-           //buttonNext.setOnClickListener {
-              // val isContinuation
-           //}
-
-         //  buttonPrevious.text = "Back"
-
-
-              /** if (mediaPlayer == null) {
-                   mediaPlayer = mediaPlayer.create(getApplicationContext(), R.raw.im_yours);
-                   mediaPlayer.start();
-               } else if(!mediaPlayer.isPlaying()) {
-                   mediaPlayer.seekTo(pausePosition);
-                   mediaPlayer.start();
-
-               }
-               break;
-
-               case R.id.pause_button:
-               if(mediaPlayer!=null) {
-                   mediaPlayer.pause();
-                   pausePosition=mediaPlayer.getCurrentPosition();
-
-               }
-               break;
-
-               case R.id.stop_button:
-               if (mediaPlayer == null) {
-                   mediaPlayer.stop();
-                   mediaPlayer=null;
-               }
-               break; **/
-
-       }
 
     }
 }
+
+
+/**
+ *     @WorkerThread
+suspend fun getVideos(): List<Video> {
+val (success, result) = NetworkAdapter.httpGetRequest(PAGE_URL)
+var videoList: List<Video>? = null
+if(success) {
+videoList = Json.parse(Video.serializer().list, result)
+}
+return videoList?: listOf()
+}
+ */
+
+
