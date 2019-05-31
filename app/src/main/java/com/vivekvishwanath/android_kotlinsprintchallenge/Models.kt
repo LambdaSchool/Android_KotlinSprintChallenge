2c0bcce28932768d5dd55a1959f611af5d62805f
VivekV95 <vivekv210@gmail.com>
2019-05-31 10:40:39 -0500

    Added dao and modified models to include optional keys

--- app/src/main/java/com/vivekvishwanath/android_kotlinsprintchallenge/Models.kt
+++ app/src/main/java/com/vivekvishwanath/android_kotlinsprintchallenge/Models.kt
@@ -5,21 +5,21 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class VideoListItem(val id: Int?, val name: String?,
-                         val news_name: String? = null, val image: String?,
-                         val collection: String?)
+                         @Optional val news_name: String? = null, val image: String?,
+                         val collection: String?): java.io.Serializable
 
 @Serializable
 data class Video(val name: String?, val short_description: String?,
-                 val youtube_id: String? = null, val credits: String?,
+                 @Optional val youtube_id: String? = null, val credits: String?,
                  val mission: String?, val collection: String?,
                  val image: String?, val image_retina: String?,
-                 val html_5_video: Html5Video? = null,
-                 val video_files: List<VideoFile>? = null)
+                 @Optional val html_5_video: Html5Video? = null,
+                 @Optional val video_files: List<VideoFile>? = null): java.io.Serializable
 
 @Serializable
-data class Html5Video(val video_url: String?, val poster_url: String?)
+data class Html5Video(val video_url: String?, val poster_url: String?): java.io.Serializable
 
 @Serializable
 data class VideoFile(val file_url: String?, val file_size: Int?,
                      val width: Int?, val height: Int?,
-                     val frame_rate: String?, val format: String?)
+                     @Optional val frame_rate: String? = null, val format: String?): java.io.Serializable
