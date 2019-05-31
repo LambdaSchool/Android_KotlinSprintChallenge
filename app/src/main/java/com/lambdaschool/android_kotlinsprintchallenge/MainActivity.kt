0e3abddc9ce7d1274474cb890dbb232bc895b6c4
heiligbasil <basilief@yahoo.com>
2019-05-31 11:12:53 -0700

    Hooked up the seek bar to control the video location & for scrubbing

--- app/src/main/java/com/lambdaschool/android_kotlinsprintchallenge/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/android_kotlinsprintchallenge/MainActivity.kt
@@ -4,19 +4,34 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.annotation.UiThread
 import android.widget.MediaController
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button_retrieve.setOnClickListener { showVideo(edit_text_retrieve.text.toString()) }
+        this.button_retrieve.setOnClickListener { showVideo(edit_text_retrieve.text.toString()) }
+
+        this.seek_bar!!.setOnSeekBarChangeListener(this)
     }
 
-    fun showVideo(videoId:String) {
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        this.video_view.seekTo(progress)
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+        seekBar?.progress?.let { this.video_view.seekTo(it) }
+    }
+
+    fun showVideo(videoId: String) {
         var videoModel: VideoModel? = null
         val job = Job()
         val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.Main + job)
@@ -30,7 +45,11 @@ class MainActivity : AppCompatActivity() {
                 video_view.setVideoPath(videoModel?.getVideoUrl())
                 mediaController?.setAnchorView(video_view)
                 video_view.setMediaController(mediaController)
-                video_view.start()
+                video_view.setOnPreparedListener {
+                    seek_bar.max = video_view.duration
+                    seek_bar.progress = 0
+                    video_view.start()
+                }
             }
         }
     }
