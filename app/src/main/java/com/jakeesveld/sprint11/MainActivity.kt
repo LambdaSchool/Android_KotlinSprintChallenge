4aef4c8576957b36b12eab9ec4e81fe7eb36206d
Jake Esveld <thejakeesveld@gmail.com>
2019-05-31 08:16:45 -0700

    Add Network Adapter

--- app/src/main/java/com/jakeesveld/sprint11/MainActivity.kt
+++ app/src/main/java/com/jakeesveld/sprint11/MainActivity.kt
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val VIDEO_URL_BASE = "http://hubblesite.org/api/v3/video"
+        const val VIDEO_LIST_URL = VIDEO_URL_BASE + "s"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/jakeesveld/sprint11/NetworkAdapter.kt
@@ -0,0 +1,59 @@
+package com.jakeesveld.sprint11
+
+import android.support.annotation.WorkerThread
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+
+object NetworkAdapter {
+
+    @WorkerThread
+    fun httpGetRequest(urlString: String): String {
+        var result = ""
+        var connection: HttpURLConnection? = null
+        var stream: InputStream? = null
+        try {
+            val url = URL(urlString)
+            connection = url.openConnection() as HttpURLConnection
+            connection.connect()
+
+            val responseCode = connection.responseCode
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                stream = connection.inputStream
+                if (stream != null) {
+                    val reader = BufferedReader(InputStreamReader(stream))
+                    val builder = StringBuilder()
+                    var line: String? = reader.readLine()
+                    while (line != null) {
+                        builder.append(line)
+                        line = reader.readLine()
+                    }
+                    result = builder.toString()
+                }
+            } else {
+                result = responseCode.toString()
+            }
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            connection?.disconnect()
+
+            if (stream != null) {
+                try {
+                    stream.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+            }
+            return result
+        }
+    }
+
+}
