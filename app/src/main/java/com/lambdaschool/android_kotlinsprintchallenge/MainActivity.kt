d3f256c07e2761e1b667c4366c5b33ead1338710
heiligbasil <basilief@yahoo.com>
2019-05-31 08:33:38 -0700

    Hardcoded test to play a video

--- app/src/main/java/com/lambdaschool/android_kotlinsprintchallenge/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/android_kotlinsprintchallenge/MainActivity.kt
@@ -2,11 +2,19 @@ package com.lambdaschool.android_kotlinsprintchallenge
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mediaController: MediaController? = MediaController(this)
+        video_view.setVideoPath("https://media.stsci.edu/uploads/video_file/video_attachment/4532/STScI-H-v1737a-640x360.mp4")
+        mediaController?.setAnchorView(video_view)
+        video_view.setMediaController(mediaController)
+        video_view.start()
     }
 }
