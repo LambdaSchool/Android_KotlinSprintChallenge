a313d767a241d00c289b9e482fcf73eb03224da8
heiligbasil <basilief@yahoo.com>
2019-05-31 08:11:00 -0700

    Added the model

---
+++ app/src/main/java/com/lambdaschool/android_kotlinsprintchallenge/HubbleVideoModel.kt
@@ -0,0 +1,33 @@
+package com.lambdaschool.android_kotlinsprintchallenge
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class VideoModel(
+    val name: String? = "",
+    val short_description: String? = "",
+    val credits: String? = "",
+    val news_name: String? = "",
+    val mission: String? = "",
+    val collection: String? = "",
+    val image: String? = "",
+    val image_retina: String? = "",
+    val html_5_video: Html_5_video?,
+    val video_files: List<VideoList>?
+)
+
+@Serializable
+data class Html_5_video(
+    val video_url: String? = "",
+    val poster_url: String? = ""
+)
+
+@Serializable
+data class VideoList(
+    val file_url: String? = "",
+    val file_size: Int? = -1,
+    val width: Int? = -1,
+    val height: Int? = -1,
+    val frame_rate: String? = "",
+    val format: String? = ""
+)
