5247aac92df55ac4b8315b4699404e065d31b045
Shoonsk8 <shoon@att.net>
2019-06-07 12:43:04 -0400

    List fragment added

--- app/src/main/java/com/shoon/android_kotlinsprintchallenge/MainActivity.kt
+++ app/src/main/java/com/shoon/android_kotlinsprintchallenge/MainActivity.kt
@@ -2,11 +2,29 @@ package com.shoon.android_kotlinsprintchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.shoon.android_kotlinsprintchallenge.dummy.DummyContent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , ItemFragment.OnListFragmentInteractionListener {
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
+
+
+    }
+
+    private lateinit var manager: FragmentManager
+    private lateinit var transaction: FragmentTransaction
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        manager = supportFragmentManager
+        transaction = manager.beginTransaction()
+        transaction.replace(R.id.frameMain, ItemFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+
     }
 }
